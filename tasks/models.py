f19920c3ad667928bc25ea0d740cd28d97d035e3
Emin <sambufer200@gmail.com>
2020-04-27 14:47:25 +0300

    create models for task tracker

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    problem = models.TextField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="team_lead")
+    status = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner_worker")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.problem
+
+
+class Definition(models.Model):
+    definition = models.TextField()
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.definition
